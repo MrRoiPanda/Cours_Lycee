12ebdebe6c2fb9dd37798f2a04b111d4e9c8812e
MrRoiPanda <61798547+MrRoiPanda@users.noreply.github.com>
2020-12-11 18:50:24 +0100

    flex

--- mp2/index.php
+++ mp2/index.php
@@ -4,23 +4,38 @@
     <meta charset="utf-8">
     <title>Dice Master</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="img/favicon.ico">
   </head>
   <body>
     <div class="content">
-      <h3>Choose the good nuber or die</h3>
-      <form class="" action="randomize.php" method="post">
-        <input id="number" type="number" name="number" value="" placeholder="0 à 9" required min="0" max="9" step="1">
-        <input id="button" type="submit" name="roll" value="roll">
-      </form>
-      <?php
-        if (isset($_COOKIE["dice"])) {
-          echo $_COOKIE["dice"];
-          echo "<br>";
-        }
-        if (isset($_COOKIE["nb"])) {
-          echo $_COOKIE["nb"];
-        }
-      ?>
+      <div class="instructions">
+        <h1>Roll the Dice</h1>
+        <p>vdgdf</p>
+        <p>dfgdgd</p>
+      </div>
+      <div class="visual">
+        <h2>
+          <?php
+            if (isset($_COOKIE["nb"])) {
+              echo $_COOKIE["nb"];
+            }
+          ?>
+        </h2>
+        <img class="icon" src="img/d10.svg" alt="">
+        <h2>
+          <?php
+            if (isset($_COOKIE["dice"])) {
+              echo $_COOKIE["dice"];
+            }
+          ?>
+        </h2>
+      </div>
+      <div class="input">
+        <form class="" action="randomize.php" method="post">
+          <input id="number" type="number" name="number" value="" placeholder="0 à 9" required min="0" max="9" step="1">
+          <input id="button" type="submit" name="roll" value="roll">
+        </form>
+      </div>
     </div>
   </body>
 </html>
--- mp2/randomize.php
+++ mp2/randomize.php
@@ -4,9 +4,12 @@
 /// Aléa [0 - 9]
 $d10 = rand (0 , 9);
 
+/// Durée de vie des cookie
+$temps = 60;
+
 /// Dice value in cookie
-setcookie("dice",$d10);
-setcookie("nb",$_POST['number']);
+setcookie("dice",$d10, time() + $temps);
+setcookie("nb",$_POST['number'], time() + $temps);
 
 header('Location:index.php');
 ?>
